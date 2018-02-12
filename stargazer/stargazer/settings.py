79110d1325409ea32505624f73b903eafd076930
Christopher Thorne <libcthorne@gmail.com>
2018-02-12 20:54:36 +0000

    Add stub toptenviewer view

--- stargazer/stargazer/settings.py
+++ stargazer/stargazer/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'toptenfetcher.apps.ToptenfetcherConfig',
+    'toptenviewer.apps.ToptenviewerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- stargazer/toptenviewer/views.py
+++ stargazer/toptenviewer/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from toptenfetcher.models import GitHubLanguage, GitHubRankedRepo
+
+
+def index(request):
+    languages = GitHubLanguage.objects.all()
+    languages_str = "<br/>".join([
+        language.language
+        for language in languages
+    ])
+
+    repos = GitHubRankedRepo.objects.all()
+    repos_str = "<br/>".join([
+        str(repo)
+        for repo in repos
+    ])
+
+    return HttpResponse(
+        "<h1>Languages</h1>{}<h1>Repos</h1>{}".format(
+            languages_str, repos_str))
