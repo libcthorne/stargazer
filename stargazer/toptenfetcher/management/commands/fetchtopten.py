4bc907d2365204759d89d1e55afeea8c980ac4ad
Christopher Thorne <libcthorne@gmail.com>
2018-02-14 20:34:03 +0000

    Add fetchtopten management command

---
+++ stargazer/toptenfetcher/management/commands/fetchtopten.py
@@ -0,0 +1,70 @@
+import os
+import sys
+
+import requests
+from django.core.management.base import BaseCommand
+
+from toptenfetcher.models import (
+    GitHubLanguage,
+    GitHubRankedRepo,
+)
+
+
+API_URL = "https://api.github.com/graphql"
+
+try:
+    OAUTH_TOKEN = os.environ["OAUTH_TOKEN"]
+except KeyError:
+    sys.stderr.write("OAUTH_TOKEN not specified\n")
+    sys.exit(1)
+
+
+class Command(BaseCommand):
+    help = "Fetches top ten GitHub repos for all registered languages"
+
+    def handle(self, *args, **options):
+        for language in GitHubLanguage.objects.all():
+            self.stdout.write("Fetching top ten repos for {}".format(language.language))
+
+            clear_top_ten_repos_for_language(language)
+            save_top_ten_repos_for_language(language)
+
+
+def clear_top_ten_repos_for_language(language):
+    GitHubRankedRepo.objects.filter(language=language).delete()
+
+def save_top_ten_repos_for_language(language):
+    r = requests.post(API_URL, json={
+        "query": """
+          query($query:String!) {
+            search(query: $query, type: REPOSITORY, first: 10) {
+              edges {
+                node {
+                  ... on Repository {
+                    description
+                    nameWithOwner
+                    url
+                    stargazers {
+                      totalCount
+                    }
+                  }
+                }
+              }
+            }
+          }
+        """,
+        "variables": {
+            "query": f"language:{language.language} sort:stars-desc"
+        }
+    }, headers={"Authorization": f"token {OAUTH_TOKEN}"})
+
+    search_results = r.json()["data"]["search"]["edges"]
+
+    for search_result in search_results:
+        repo_data = search_result["node"]
+        repo = GitHubRankedRepo(
+            name=repo_data["nameWithOwner"],
+            stargazers=repo_data["stargazers"]["totalCount"],
+            language=language,
+        )
+        repo.save()
