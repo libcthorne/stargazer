0421be6e2a95e133e5c01db82828912ff2a6ab47
Christopher Thorne <libcthorne@gmail.com>
2018-02-17 10:34:20 +0000

    Move index into template

--- stargazer/viewer/views.py
+++ stargazer/viewer/views.py
@@ -1,21 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from fetcher.models import GitHubLanguage, GitHubRankedRepo
 
 
 def index(request):
-    languages = GitHubLanguage.objects.all()
-    languages_str = "<br/>".join([
-        language.name
-        for language in languages
-    ])
-
-    repos = GitHubRankedRepo.objects.all()
-    repos_str = "<br/>".join([
-        str(repo)
-        for repo in repos
-    ])
-
-    return HttpResponse(
-        "<h1>Languages</h1>{}<h1>Repos</h1>{}".format(
-            languages_str, repos_str))
+    return render(request, "viewer/index.html", {
+        "languages": GitHubLanguage.objects.all(),
+        "repos": GitHubRankedRepo.objects.all(),
+    })
