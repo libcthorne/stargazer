96d4245c24c2148fe2e6410db95e59af51893144
Christopher Thorne <libcthorne@gmail.com>
2018-02-17 12:09:02 +0000

    Implement basic search functionality

--- stargazer/viewer/urls.py
+++ stargazer/viewer/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('repos/', views.repos_show, name='repos_show'),
 ]
--- stargazer/viewer/views.py
+++ stargazer/viewer/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from fetcher.models import GitHubLanguage, GitHubRankedRepo
 
 
 def index(request):
     return render(request, "viewer/index.html")
+
+def repos_show(request):
+    language_name = request.GET.get("language")
+    language = get_object_or_404(
+        GitHubLanguage,
+        name__iexact=language_name,
+    )
+    repos = GitHubRankedRepo.objects.filter(
+        language=language,
+    )
+
+    return render(request, "viewer/repos_show.html", {
+        "language": language,
+        "repos": repos,
+    })
