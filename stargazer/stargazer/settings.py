c89a266640cecb063f308b356df0bb30c0cd9b1d
Christopher Thorne <libcthorne@gmail.com>
2018-02-21 19:50:19 +0000

    Add dj-database-url to enable use of DATABASE_URL envvar

--- stargazer/stargazer/settings.py
+++ stargazer/stargazer/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -79,10 +81,9 @@ WSGI_APPLICATION = 'stargazer.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    ),
 }
 
 
